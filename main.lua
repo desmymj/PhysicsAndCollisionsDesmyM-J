7bfc26431e958d993e391289e6fa8fd7bfc13c73
Desmy MJ <desmy.michaelis-joseph@ocsbstudent.ca>
2018-03-29 10:59:02 -0400

    added code

--- main.lua
+++ main.lua
@@ -5,3 +5,11 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+--Hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--load physics
+local physics = require("physics")
+
+-- start physics
